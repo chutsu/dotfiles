8533afe11e751b602e1093cafa0a4c0566cf7fb3
Christopher Choi <chutsu@gmail.com>
2012-02-15 23:04:56 +0000

    Modified mkln to remove old dotfiles before symlinking new ones

--- mkln.sh
+++ mkln.sh
@@ -1,6 +1,10 @@
 #/bin/sh
+echo "Removed old dotfiles"
+rm -rf $HOME/.vim
+rm $HOME/.vimrc
+rm $HOME/.bash_profile
 
-echo "Creating symlinks for the dotfiles"
+echo "Created symlinks for the dotfiles"
 ln -s $PWD/vim $HOME/.vim
 ln -s $PWD/vimrc $HOME/.vimrc
 ln -s $PWD/bash_profile $HOME/.bash_profile
