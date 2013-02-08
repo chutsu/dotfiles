06f326bfdd3e752bb353caa6b63191525eadff94
Chris Choi <chutsu@gmail.com>
2013-02-08 16:59:03 +0000

    added tmux config

--- init.sh
+++ init.sh
@@ -3,11 +3,13 @@ echo "Removed old dotfiles"
 rm -rf $HOME/.vim
 rm $HOME/.vimrc
 rm $HOME/.bash_profile
+rm $HOME/.tmux.conf
 
 echo "Created symlinks for the dotfiles"
 ln -s $PWD/vim $HOME/.vim
 ln -s $PWD/vimrc $HOME/.vimrc
 ln -s $PWD/bash_profile $HOME/.bash_profile
+ln -s $PWD/tmux.conf $HOME/.tmux.conf
 
 echo "Initiate vim modules"
 git submodule init
