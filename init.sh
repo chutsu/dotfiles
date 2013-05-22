6b0884e504420d3b9424773ebd21625bc90fe5ad
Chris Choi <chutsu@gmail.com>
2013-05-22 11:57:42 +0100

    Add vim bundle install into init.sh process

--- init.sh
+++ init.sh
@@ -15,14 +15,15 @@ rm $HOME/.vimrc
 rm $HOME/.bash_profile
 rm $HOME/.tmux.conf
 
-echo "Install vim plugins"
-git clone https://github.com/gmarik/vundle.git $PWD/vim/bundle/vundle
-
 echo "Created symlinks for the dotfiles"
 ln -s $PWD/vim $HOME/.vim
 ln -s $PWD/vimrc $HOME/.vimrc
 ln -s $PWD/bash_profile $HOME/.bash_profile
 ln -s $PWD/tmux.conf $HOME/.tmux.conf
 ln -s $PWD/inputrc $HOME/.inputrc
+
+echo "Install vim plugins"
+git clone https://github.com/gmarik/vundle.git $PWD/vim/bundle/vundle
+vim +BundleInstall +qall
 
 echo "Done! :)"
