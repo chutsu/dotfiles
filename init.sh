e017f8dcfac4baec36318e4f0ccd429bb67a0a05
Chris Choi <chutsu@gmail.com>
2013-06-19 22:26:06 +0100

    Add error surpression to init.sh

--- init.sh
+++ init.sh
@@ -11,16 +11,16 @@ esac
 
 echo "Removed old dotfiles"
 rm -rf $HOME/.vim
-rm $HOME/.vimrc
-rm $HOME/.bash_profile
-rm $HOME/.tmux.conf
+rm -f $HOME/.vimrc
+rm -f $HOME/.bash_profile
+rm -f $HOME/.tmux.conf
 
 echo "Created symlinks for the dotfiles"
-ln -s $PWD/vim $HOME/.vim
-ln -s $PWD/vimrc $HOME/.vimrc
-ln -s $PWD/bash_profile $HOME/.bash_profile
-ln -s $PWD/tmux.conf $HOME/.tmux.conf
-ln -s $PWD/inputrc $HOME/.inputrc
+ln -fs $PWD/vim $HOME/.vim
+ln -fs $PWD/vimrc $HOME/.vimrc
+ln -fs $PWD/bash_profile $HOME/.bash_profile
+ln -fs $PWD/tmux.conf $HOME/.tmux.conf
+ln -fs $PWD/inputrc $HOME/.inputrc
 
 echo "Install vim plugins"
 git clone https://github.com/gmarik/vundle.git $PWD/vim/bundle/vundle
