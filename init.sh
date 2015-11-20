5965fb2f5431fce63e5c81361c8794487acb56f9
Chris Choi <chutsu@gmail.com>
2015-11-19 20:42:25 -0500

    Update dotfiles structure

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #/bin/sh
-# DETERMINE OS
-case $( uname -s ) in
+
+# determine os
+case $(uname -s) in
 Linux)
     OS="LINUX"
     ;;
@@ -9,39 +10,54 @@ Darwin)
     ;;
 esac
 
-# REMOVE OLD DOTFILES
-echo "Removed old dotfiles"
-rm -rf $HOME/.vim
-rm -f $HOME/.vimrc
-rm -f $HOME/.bash_profile
-rm -f $HOME/.tmux.conf
-rm -rf $HOME/.i3
-rm -f $HOME/.xinitrc
-rm -f $HOME/.Xdefaults
-rm -f $HOME/.muttrc
-rm -rf $HOME/.mutt
-rm -f $HOME/.xbindkeysrc
 
-# SYMLINKS
-echo "Created symlinks for the dotfiles"
-ln -fs $PWD/vim $HOME/.vim
-ln -fs $PWD/vimrc $HOME/.vimrc
-ln -fs $PWD/bash_profile $HOME/.bash_profile
-ln -fs $PWD/tmux.conf $HOME/.tmux.conf
-ln -fs $PWD/inputrc $HOME/.inputrc
-ln -fs $PWD/i3 $HOME/.i3
-ln -fs $PWD/xinitrc $HOME/.xinitrc
-ln -fs $PWD/Xdefaults $HOME/.Xdefaults
-ln -fs $PWD/muttrc $HOME/.muttrc
-ln -fs $PWD/mutt $HOME/.mutt
-ln -fs $PWD/vimperatorrc $HOME/.vimperatorrc
-ln -fs $PWD/xbindkeysrc $HOME/.xbindkeysrc
+init_dotfiles()
+{
+    # REMOVE OLD DOTFILES
+    echo "remove old dotfiles"
+    rm -rf $HOME/.vim
+    rm -f $HOME/.vimrc
+    rm -f $HOME/.bash_profile
+    rm -f $HOME/.tmux.conf
+    rm -rf $HOME/.i3
+    rm -f $HOME/.xinitrc
+    rm -f $HOME/.Xdefaults
+    rm -f $HOME/.muttrc
+    rm -rf $HOME/.mutt
+    rm -f $HOME/.xbindkeysrc
 
-echo "Install vim plugins"
-git submodule init
-git submodule update
-vim -c VundleInstall -c quitall
-./vim/bundle/YouCompleteMe/install.sh --clang-completer
-exec ./vim/bundle/fzf/install
+    # SYMLINKS
+    echo "creating symlinks for the dotfiles"
+    ln -fs $PWD/vim $HOME/.vim
+    ln -fs $PWD/vim/vimrc $HOME/.vimrc
+    ln -fs $PWD/tmux/tmux.conf $HOME/.tmux.conf
+    ln -fs $PWD/i3 $HOME/.i3
+    ln -fs $PWD/configs/inputrc $HOME/.inputrc
+    ln -fs $PWD/configs/xinitrc $HOME/.xinitrc
+    ln -fs $PWD/configs/Xdefaults $HOME/.Xdefaults
+    ln -fs $PWD/configs/xbindkeysrc $HOME/.xbindkeysrc
+    ln -fs $PWD/configs/vimperatorrc $HOME/.vimperatorrc
+    ln -fs $PWD/configs/bash_profile $HOME/.bash_profile
 
-echo "Done! :)"
+    return 0;
+}
+
+init_vim()
+{
+    echo "install vim plugins"
+    git submodule init
+    git submodule update
+    vim -c VundleInstall -c quitall
+    exec ./vim/bundle/fzf/install --all
+
+    return 0;
+}
+
+init()
+{
+    init_dotfiles
+    init_vim
+    echo "Done! :)"
+}
+
+init
