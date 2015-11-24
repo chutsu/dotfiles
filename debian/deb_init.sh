0838fef8a58f5860060829415a398619e4e76ebb
chutsu <chutsu@gmail.com>
2015-11-23 21:21:26 -0500

    Configure apt-get in bashrc

--- debian/deb_init.sh
+++ debian/deb_init.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+BASEDIR=$(dirname $0)
+echo $BASEDIR
 
-ln -fs $PWD/configs/sudoers /etc/sudoers
-ln -fs $PWD/configs/sources.list /etc/apt/sources.list
+ln -fs $BASEDIR/configs/sudoers /etc/sudoers
+ln -fs $BASEDIR/configs/sources.list /etc/apt/sources.list
 
 apt-get update
-cat deb.pkgs | xargs apt-get install -y
+grep -v '^#' $BASEDIR/deb.pkgs | xargs apt-get install -y
 
 # rm -rf Desktop
 # rm -rf Documents
