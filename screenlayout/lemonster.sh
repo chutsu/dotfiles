eb6cfc71fed909f3385df78f446f1ec158fc8bbb
Chris Choi <chutsu@gmail.com>
2016-07-30 12:34:04 -0400

    Update

--- screenlayout/lemonster.sh
+++ screenlayout/lemonster.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 xrandr \
-    --output DVI-I-1 --primary --mode 1920x1080 --pos 0x0 --rotate right \
-    --output DVI-I-2 --mode 1920x1080 --pos 1080x456 --rotate normal
+    --output DVI-D-0 --primary --mode 1920x1080 --pos 0x0 --rotate right \
+    --output DVI-I-1 --mode 1920x1080 --pos 1080x456 --rotate normal
---
+++ scripts/c_init.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e  # halt on first error
+
+mkdir src
+mkdir tests
+mkdir include
+
+touch README.md
+touch config.mk
+touch Makefile
+touch src/Makefile
+touch tests/Makefile
