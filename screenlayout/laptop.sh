e9436d099cb26a47b0c60533b2f89268c6c3a703
Chris Choi <chutsu@gmail.com>
2018-11-18 15:58:15 +0000

    Add screenlayout configs

---
+++ screenlayout/laptop.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+xrandr \
+  --output HDMI-0 --off \
+  --output DP-4 --off \
+  --output DP-3 --off \
+  --output DP-2 \
+    --mode 1920x1080 \
+    --pos 0x0 \
+    --rotate normal \
+  --output DP-1 --off \
+  --output DP-0 --off
--- screenlayout/lemonster.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DVI-I-1 --mode 1920x1080 --pos 0x0 --rotate right --output HDMI-1 --off --output DP-1 --off --output DVI-D-1 --mode 1920x1080 --pos 1080x440 --rotate normal
