47d508cece704944a72fa1965ed0fd94548c95a6
Chris Choi <chutsu@gmail.com>
2020-06-11 11:34:30 +0100

    Update work screen layout

--- screenlayout/work.sh
+++ screenlayout/work.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-0 --mode 1920x1200 --pos 1920x0 --rotate normal --output DP-4 --off --output DP-3 --off --output DP-2 --primary --mode 1920x1080 --pos 0x120 --rotate normal --output DP-1 --off --output DP-0 --off
+xrandr --output HDMI-0 --mode 1920x1200 --pos 1920x0 --rotate normal --output DP-4 --off --output DP-3 --off --output DP-2 --primary --mode 1920x1080 --pos 0x56 --rotate normal --output DP-1 --off --output DP-0 --off
