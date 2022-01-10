132b1b17cdf5f85c421f91d515b508a51205cedc
Chris Choi <chutsu@gmail.com>
2022-01-10 10:37:17 +0000

    Update screen settings

--- screenlayout/work.sh
+++ screenlayout/work.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1-1 --mode 1920x1080 --pos 0x360 --rotate normal --output HDMI-0 --mode 2560x1440 --pos 1920x0 --rotate normal --output DP-3 --off --output DP-2 --off --output DP-1 --off --output DP-0 --off
+xrandr --output DP-1 --off --output DP-3 --off --output HDMI-1 --mode 1920x1200 --pos 0x0 --rotate left --output eDP-1 --primary --mode 1920x1080 --pos 1200x840 --rotate normal --output DP-2 --off
