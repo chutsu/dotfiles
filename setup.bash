8ea3137b3cb6a9541f994da7ab76a54b2fa00df8
Chris Choi <chutsu@gmail.com>
2022-04-27 16:08:01 +0100

    Add ffmpegthumbnailer and mplayer to setup.bash

--- setup.bash
+++ setup.bash
@@ -42,7 +42,12 @@ install_dev_pkgs() {
 
   # Vifm
   pip3 install ueberzug
+  $APT_INSTALL ffmpegthumbnailer
 
+  # MISC
+  $APT_INSTALL mplayer
+
+  # NVIM - vim-plug
   sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 }
