6493daae3311dd722e651dd511672e131375e1fd
Chris Choi <chutsu@gmail.com>
2025-09-14 13:13:13 +0100

    Add gr keyboard shortcut for getting all refernces to function

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -146,6 +146,7 @@ vim.opt.statusline = status_line()
 
 
 -- LSP Config
+vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
 vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
 vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
